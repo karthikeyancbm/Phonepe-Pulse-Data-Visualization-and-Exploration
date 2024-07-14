f706c73023d6ebf2ea6a27c289f96df265e059d8
Karthikeyan M <153653235+karthikeyancbm@users.noreply.github.com>
2024-07-14 19:13:41 +0530

    Update Data Extraction.py

--- Data Extraction.py
+++ Data Extraction.py
@@ -21,7 +21,7 @@ def agg_trans_list():
     path1 = "C:/Users/ASUS/Documents/GUVI ZEN CLASSES/MAINT BOOT/PHONE PE PROJECT/pulse/data/aggregated/transaction/country/india/state/"
     agg_trans_list = os.listdir(path1)
 
-    clm={'State':[], 'Year':[],'Quarter':[],'Transacion_type':[], 'Transacion_count':[], 'Transacion_amount':[]}
+    clm={'State':[], 'Year':[],'Quarter':[],'Transaction_type':[], 'Transaction_count':[], 'Transaction_amount':[]}
 
     for state in agg_trans_list:
         updated_state_list = path1+state+"/"
@@ -38,22 +38,26 @@ def agg_trans_list():
                 for z in A['data']['transactionData']:
                     Name=z['name']
                     count=z['paymentInstruments'][0]['count']
+                    count_in_cr = count/10_000_000
+                    count_round = round(count_in_cr,2)
                     amount=z['paymentInstruments'][0]['amount']
-                    clm['Transacion_type'].append(Name)
-                    clm['Transacion_count'].append(count)
-                    clm['Transacion_amount'].append(amount)
+                    amount_in_cr = amount/10_000_000
+                    amount_round = round(amount_in_cr,2)
+                    clm['Transaction_type'].append(Name)
+                    clm['Transaction_count'].append(count_round)
+                    clm['Transaction_amount'].append(amount_round)
                     clm['State'].append(state)
                     clm['Year'].append(year)
                     clm['Quarter'].append(int(file.strip('.json')))
 
     Agg_Trans=pd.DataFrame(clm)
-    Agg_Trans['Transacion_amount']=Agg_Trans['Transacion_amount'].astype(int) 
+    
 
     Agg_Trans["State"] = Agg_Trans["State"].str.replace('andaman-&-nicobar-islands','Andaman & nicobar')
     Agg_Trans["State"] = Agg_Trans["State"].str.replace("-"," ")
     Agg_Trans["State"] = Agg_Trans["State"].str.title()
-    Agg_Trans["State"] = Agg_Trans["State"].str.replace('Dadra & Nagar Haveli & Daman & Diu','Dadra and Nagar Haveli and Daman and Diu')
-
+    Agg_Trans["State"] = Agg_Trans["State"].str.replace('Dadra & Nagar Haveli & Daman & Diu','Dadra and Nagar Haveli and Daman and Diu') 
+        
     return Agg_Trans
 
 agg_trans_list()
@@ -100,7 +104,8 @@ def aggregate_user():
     Aggregate_user["State"] = Aggregate_user["State"].str.replace("-"," ")
     Aggregate_user["State"] = Aggregate_user["State"].str.title()
     Aggregate_user["State"] = Aggregate_user["State"].str.replace('Dadra & Nagar Haveli & Daman & Diu','Dadra and Nagar Haveli and Daman and Diu')
-
+    Aggregate_user.rename(columns={'State':'STATE','Year':'YEAR','Quarter':'QUARTER','Brands':'BRANDS','Count':'COUNT','Percentage':'PERCENTAGE'},inplace=True) 
+         
     return Aggregate_user
 
 aggregate_user()
@@ -112,7 +117,7 @@ def map_transaction():
     path3= "C:/Users/ASUS/Documents/GUVI ZEN CLASSES/MAINT BOOT/PHONE PE PROJECT/pulse/data/map/transaction/hover/country/india/state/"
     map_trans_list = os.listdir(path3)
 
-    clm3={'State':[], 'Year':[],'Quarter':[],'Districts':[], 'Transaction_count':[],'Transaction_amount':[]}
+    clm3={'State':[], 'Year':[],'Quarter':[],'Districts':[], 'Transaction_Count':[],'Transaction_Amount':[]}
 
     for state in map_trans_list:
         updated_state_list = path3+state+"/"
@@ -129,20 +134,28 @@ def map_transaction():
                 for i in C['data']['hoverDataList']:
                         district=i["name"]
                         count=i['metric'][0]['count']
+                        count_in_cr = count/10_000_000
+                        count_round = round(count_in_cr,2)
                         amount=i['metric'][0]['amount']
+                        amount_in_cr = amount/10_000_000
+                        amount_round = round(amount_in_cr,2)
                         clm3['Districts'].append(district)
-                        clm3['Transaction_count'].append(count)
-                        clm3['Transaction_amount'].append(amount)
+                        clm3['Transaction_Count'].append(count_round)
+                        clm3['Transaction_Amount'].append(amount_round)
                         clm3['State'].append(state)
                         clm3['Year'].append(year)
                         clm3['Quarter'].append(int(file.strip('.json')))
     Map_trans=pd.DataFrame(clm3)
-    Map_trans['Transaction_amount']=Map_trans['Transaction_amount'].astype(int)
+
 
     Map_trans["State"] = Map_trans["State"].str.replace('andaman-&-nicobar-islands','Andaman & nicobar')
     Map_trans["State"] = Map_trans["State"].str.replace("-"," ")
     Map_trans["State"] = Map_trans["State"].str.title()
     Map_trans["State"] = Map_trans["State"].str.replace('Dadra & Nagar Haveli & Daman & Diu','Dadra and Nagar Haveli and Daman and Diu')
+    Map_trans.rename(columns={'State':'STATE','Year':'YEAR','Quarter':'QUARTER','Districts':'DISTRICTS','Transaction_Count':'TRANSACTION_COUNT',
+                              'Transaction_Amount':'TRANSACTION_AMOUNT'},inplace=True)          
+
+
     return Map_trans
 
 map_transaction()       
@@ -189,7 +202,9 @@ def map_user():
     Map_user["State"] = Map_user["State"].str.replace("-"," ")
     Map_user["State"] = Map_user["State"].str.title()
     Map_user["State"] = Map_user["State"].str.replace('Dadra & Nagar Haveli & Daman & Diu','Dadra and Nagar Haveli and Daman and Diu')
-    
+    Map_user.rename(columns={'State':'STATE','Year':'YEAR','Quarter':'QUARTER','Districts':'DISTRICTS',
+                             'Registered_users':'REGISTERED_USERS','AppOpens':'APP_OPENS'},inplace=True)
+                
     return Map_user
 
 map_user()   
@@ -218,21 +233,27 @@ def top_transaction():
                 for i in E['data']['pincodes']:
                     pincodes=i['entityName']
                     count=i['metric']["count"]
+                    count_in_cr = count/10_000_000
+                    count_round = round(count_in_cr,2)
                     amount=i['metric']['amount']
+                    amount_in_cr = amount/10_000_000
+                    amount_round = round(amount_in_cr,2)
                     clm5['Pincodes'].append(pincodes)
-                    clm5['Transaction_count'].append(count)
-                    clm5['Transaction_amount'].append(amount)
+                    clm5['Transaction_count'].append(count_round)
+                    clm5['Transaction_amount'].append(amount_round)
                     clm5['State'].append(state)
                     clm5['Year'].append(year)
                     clm5['Quarter'].append(int(file.strip('.json')))
 
     top_trans=pd.DataFrame(clm5)
-    top_trans['Transaction_amount']=top_trans['Transaction_amount'].astype(int)
+
 
     top_trans["State"] = top_trans["State"].str.replace('andaman-&-nicobar-islands','Andaman & nicobar')
     top_trans["State"] = top_trans["State"].str.replace("-"," ")
     top_trans["State"] = top_trans["State"].str.title()
     top_trans["State"] = top_trans["State"].str.replace('Dadra & Nagar Haveli & Daman & Diu','Dadra and Nagar Haveli and Daman and Diu')
+    top_trans.rename(columns={'State':'STATE','Year':'YEAR','Quarter':'QUARTER','Pincodes':'PINCODES',
+                              'Transaction_count':'TRANSACTION_COUNT','Transaction_amount':'TRANSACTION_AMOUNT IN CR'},inplace=True)
 
     return top_trans
 
@@ -274,6 +295,9 @@ def top_user():
     top_user["State"] = top_user["State"].str.replace("-"," ")
     top_user["State"] = top_user["State"].str.title()
     top_user["State"] = top_user["State"].str.replace('Dadra & Nagar Haveli & Daman & Diu','Dadra and Nagar Haveli and Daman and Diu')
+    top_user.rename(columns={'State':'STATE','Year':'YEAR','Quarter':'QUARTER',
+                             'Pincodes':'PINCODES','Registered_users':'REGISTERED_USERS'},inplace=True)
+
 
     return top_user
 
@@ -288,25 +312,23 @@ def Aggregate_transaction_table():
 
     mycursor = connection.cursor()
 
-    # Aggregate_transaction table creation
 
-    query_1 = '''CREATE TABLE IF NOT EXISTS Aggregate_transaction(State varchar(150),Year int,
-                                                    Quarter int,Transacion_type varchar(200),
-                                                    Transacion_count int,Transacion_amount bigint)'''
+
+    query_1 = '''CREATE TABLE IF NOT EXISTS Aggregate_transaction(STATE varchar(150),YEAR int,
+                                                    QUARTER int,TRANSACTION_TYPE varchar(200),
+                                                    TRANSACTION_COUNT int,TRANSACTION_AMOUNT bigint)'''
 
 
     mycursor.execute(query_1)
     connection.commit()
 
-    
-
     for index,row in agg_trans_list().iterrows():
-        insert_query_1 = '''insert into Aggregate_transaction(State,Year,
-                                                    Quarter,Transacion_type,Transacion_count,
-                                                    Transacion_amount)
+        insert_query_1 = '''insert into Aggregate_transaction(STATE,YEAR,
+                                                    QUARTER,TRANSACTION_TYPE,TRANSACTION_COUNT,
+                                                    TRANSACTION_AMOUNT)
 
                                         values(%s,%s,%s,%s,%s,%s)'''
-        rows=(row['State'],row['Year'],row['Quarter'],row['Transacion_type'],row['Transacion_count'],row['Transacion_amount'])
+        rows=(row['STATE'],row['YEAR'],row['QUARTER'],row['TRANSACTION_TYPE'],row['TRANSACTION_COUNT'],row['TRANSACTION_AMOUNT'])
 
         mycursor.execute(insert_query_1,rows)
         connection.commit()
@@ -322,30 +344,30 @@ def aggregate_user_table():
 
     mycursor = connection.cursor()
 
-    # Aggregate_user table creation
+# Aggregate_user table creation
 
-    query_1 = '''CREATE TABLE IF NOT EXISTS Aggregate_user(State varchar(150),Year int,
-                                                    Quarter int,Brands varchar(150),Count int,
-                                                    Percentage float)'''
+    query_1 = '''CREATE TABLE IF NOT EXISTS Aggregate_user(STATE varchar(150),YEAR int,
+                                                QUARTER int,BRANDS varchar(150),COUNT int,
+                                                PERCENTAGE float)'''
 
 
     mycursor.execute(query_1)
     connection.commit()
 
     for index,row in aggregate_user().iterrows():
-        insert_query_1 = '''insert into Aggregate_user(State,Year,
-                                                    Quarter,Brands,Count,
-                                                    Percentage)
+        insert_query_1 = '''insert into Aggregate_user(STATE,YEAR,
+                                                QUARTER,BRANDS,COUNT,
+                                                PERCENTAGE)
 
-                                        values(%s,%s,%s,%s,%s,%s)'''
-        rows=(row['State'],row['Year'],row['Quarter'],row['Brands'],row['Count'],row['Percentage'])
+                                    values(%s,%s,%s,%s,%s,%s)'''
+        rows=(row['STATE'],row['YEAR'],row['QUARTER'],row['BRANDS'],row['COUNT'],row['PERCENTAGE'])
 
         mycursor.execute(insert_query_1,rows)
         connection.commit()
 
 aggregate_user_table()
 
-# Table creation
+#Table creation
 
 def map_transaction_table():
 
@@ -354,23 +376,23 @@ def map_transaction_table():
 
     mycursor = connection.cursor()
 
-    # Map_transaction table creation
+# Map_transaction table creation
 
-    query_1 = '''CREATE TABLE IF NOT EXISTS map_transaction(State varchar(150),Year int,
-                                                    Quarter int,Districts varchar(150),Transaction_count int,
-                                                    Transaction_amount bigint)'''
+    query_1 = '''CREATE TABLE IF NOT EXISTS map_transaction(STATE varchar(150),YEAR int,
+                                                QUARTER int,DISTRICTS varchar(150),TRANSACTION_COUNT int,
+                                                TRANSACTION_AMOUNT bigint)'''
 
 
     mycursor.execute(query_1)
     connection.commit()
 
-    for index,row in map_transaction().iterrows():
-        insert_query_1 = '''insert into map_transaction(State,Year,
-                                                    Quarter,Districts,Transaction_count,
-                                                    Transaction_amount)
+    for index,row in map_transaction() .iterrows():
+        insert_query_1 = '''insert into map_transaction(STATE,YEAR,
+                                                QUARTER,DISTRICTS,TRANSACTION_COUNT,
+                                                TRANSACTION_AMOUNT)
 
-                                        values(%s,%s,%s,%s,%s,%s)'''
-        rows=(row['State'],row['Year'],row['Quarter'],row['Districts'],row['Transaction_count'],row['Transaction_amount'])
+                                    values(%s,%s,%s,%s,%s,%s)'''
+        rows=(row['STATE'],row['YEAR'],row['QUARTER'],row['DISTRICTS'],row['TRANSACTION_COUNT'],row['TRANSACTION_AMOUNT'])
 
         mycursor.execute(insert_query_1,rows)
         connection.commit()
@@ -386,54 +408,56 @@ def map_user_table():
 
     mycursor = connection.cursor()
 
-    # Map_user table creation
+# Map_user table creation
 
-    query_1 = '''CREATE TABLE IF NOT EXISTS map_user(State varchar(150),Year int,
-                                                    Quarter int,Districts varchar(150),Registered_users int,
-                                                    AppOpens bigint)'''
+    query_1 = '''CREATE TABLE IF NOT EXISTS map_user(STATE varchar(150),YEAR int,
+                                                QUARTER int,DISTRICTS varchar(150),REGISTERED_USERS int,
+                                                APP_OPENS bigint)'''
 
 
     mycursor.execute(query_1)
     connection.commit()
 
     for index,row in map_user().iterrows():
-        insert_query_1 = '''insert into map_user(State,Year,
-                                                    Quarter,Districts,Registered_users,
-                                                    AppOpens)
+        insert_query_1 = '''insert into map_user(STATE,YEAR,
+                                                QUARTER,DISTRICTS,REGISTERED_USERS,
+                                                APP_OPENS)
 
-                                        values(%s,%s,%s,%s,%s,%s)'''
-        rows=(row['State'],row['Year'],row['Quarter'],row['Districts'],row['Registered_users'],row['AppOpens'])
+                                    values(%s,%s,%s,%s,%s,%s)'''
+        rows=(row['STATE'],row['YEAR'],row['QUARTER'],row['DISTRICTS'],row['REGISTERED_USERS'],row['APP_OPENS'])
 
         mycursor.execute(insert_query_1,rows)
         connection.commit()
+
 
 map_user_table()
 
 # Table creation
 
 def top_transaction_table():
+
     connection = mysql.connector.connect(host="localhost",user="root",password="Bairavi@17",database="phonepe_data")
     connection
 
     mycursor = connection.cursor()
 
-    # Top_transaction table creation
+# Top_transaction table creation
 
-    query_1 = '''CREATE TABLE IF NOT EXISTS top_tranaction(State varchar(150),Year int,
-                                                    Quarter int,Pincodes varchar(150),Transaction_count int,
-                                                    Transaction_amount bigint)'''
+    query_1 = '''CREATE TABLE IF NOT EXISTS top_tranaction(STATE varchar(150),YEAR int,
+                                                QUARTER int,PINCODES varchar(150),TRANSACTION_COUNT int,
+                                                TRANSACTION_AMOUNT bigint)'''
 
 
     mycursor.execute(query_1)
     connection.commit()
 
     for index,row in top_transaction().iterrows():
-        insert_query_1 = '''insert into top_tranaction(State,Year,
-                                                    Quarter,Pincodes,Transaction_count,
-                                                    Transaction_amount)
+        insert_query_1 = '''insert into top_tranaction(STATE,YEAR,
+                                                QUARTER,PINCODES,TRANSACTION_COUNT,
+                                                TRANSACTION_AMOUNT)
 
-                                        values(%s,%s,%s,%s,%s,%s)'''
-        rows=(row['State'],row['Year'],row['Quarter'],row['Pincodes'],row['Transaction_count'],row['Transaction_amount'])
+                                    values(%s,%s,%s,%s,%s,%s)'''
+        rows=(row['STATE'],row['YEAR'],row['QUARTER'],row['PINCODES'],row['TRANSACTION_COUNT'],row['TRANSACTION_AMOUNT'])
 
         mycursor.execute(insert_query_1,rows)
         connection.commit()
@@ -449,26 +473,27 @@ def top_user_table():
 
     mycursor = connection.cursor()
 
-    # Top_user table creation
+# Top_user table creation
 
-    query_1 = '''CREATE TABLE IF NOT EXISTS top_user(State varchar(150),Year int,
-                                                    Quarter int,Pincodes varchar(150),Registered_users int
-                                                    )'''
+    query_1 = '''CREATE TABLE IF NOT EXISTS top_user(STATE varchar(150),YEAR int,
+                                                QUARTER int,PINCODES varchar(150),REGISTERED_USERS int
+                                                )'''
 
 
     mycursor.execute(query_1)
     connection.commit()
 
     for index,row in top_user().iterrows():
-        insert_query_1 = '''insert into top_user(State,Year,
-                                                    Quarter,Pincodes,Registered_users
-                                                    )
+        insert_query_1 = '''insert into top_user(STATE,YEAR,
+                                                QUARTER,PINCODES,REGISTERED_USERS
+                                                )
 
-                                        values(%s,%s,%s,%s,%s)'''
-        rows=(row['State'],row['Year'],row['Quarter'],row['Pincodes'],row['Registered_users'])
+                                    values(%s,%s,%s,%s,%s)'''
+        rows=(row['STATE'],row['YEAR'],row['QUARTER'],row['PINCODES'],row['REGISTERED_USERS'])
 
         mycursor.execute(insert_query_1,rows)
         connection.commit()
+
 
 top_user_table()
 
